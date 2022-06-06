7338b6b908bbdfc755e6e1fa773bf49db85473e7
Mohamedlee27 <leonk8070@gmail.com>
2022-06-06 19:43:53 +0300

    post config

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+	image = models.ImageField()
+	description = models.TextField()
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
