c1605ac6a0b0670859c321ffb2faf59b8b8a4cdc
Mohamedlee27 <leonk8070@gmail.com>
2022-06-06 19:49:57 +0300

    created several imports in views.py

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView
+from django.views.generic.detail import DetailView
+from django.urls import reverse
+from django import forms
+
+from posts.models import Post, Comment
 
 # Create your views here.
