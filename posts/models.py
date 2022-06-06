3aa50142de73c4760bf9d87ef031381785285355
Mohamedlee27 <leonk8070@gmail.com>
2022-06-06 19:46:08 +0300

    created Comment class model

--- posts/models.py
+++ posts/models.py
@@ -9,3 +9,10 @@ class Post(models.Model):
 	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	created = models.DateTimeField(auto_now_add=True)
 	modified = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+	post = models.ForeignKey(Post, on_delete=models.CASCADE)
+	text = models.TextField()
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
+	created = models.DateTimeField(auto_now_add=True)
+	modified = models.DateTimeField(auto_now=True)
