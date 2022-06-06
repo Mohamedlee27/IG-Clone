a5146eabd0410e5c12c0ce406f43a0a91d644181
Mohamedlee27 <leonk8070@gmail.com>
2022-06-06 21:58:48 +0300

    deployment

--- posts/views.py
+++ posts/views.py
@@ -29,17 +29,17 @@ class PostDetail(DetailView):
 		return context
 
 def post(self, request, *args, **kwargs):
-    comment_form = CommentForm(request.POST)
-    if comment_form.is_valid():
-        comment = Comment(
-            author=request.user,
-            post=self.get_object(),
-            text=comment_form.cleaned_data['comment']
-        )
-        comment.save
-    else:
-        raise Exception
-    return redirect(reverse('detail', args=[self.get_object().id]))
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            comment = Comment(
+                author=request.user,
+                post=self.get_object(),
+                text=comment_form.cleaned_data['comment']
+            )
+            comment.save
+        else:
+            raise Exception
+        return redirect(reverse('detail', args=[self.get_object().id]))
 
 
          
